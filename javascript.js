496fd0b86514227d854d6f19c54212d8d0d3505e
SenthilKumar M <56290175+senthilkumar909542@users.noreply.github.com>
2023-09-21 15:04:00 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,43 +1,49 @@
 let array = document.querySelectorAll(".data>div");
 let input = document.getElementById('input');
 
-let output = '';
+let output = '0';
 
-
+input.innerHTML = output;
 array.forEach(x => x.addEventListener('click', clicked));
 
 function clicked(event) {
 
     let x = event.target.innerText;
-    console.log(output.length);
 
-    if (x === "=" && output.length <= 10) {
-        output = eval(output);
+
+    if (x === "=") {
+        let result;
+
+        try {
+            result = String(eval(output));
+        } catch {
+            input.innerHTML = "Invalid Input";
+        }
+        output = result.slice(-1) == "=" ? result.slice(0, result.length - 1) : result;
+
+        if (output === "undefined") output = "";
+
+        input.innerHTML = output;
+
     } else if (x === "C") {
+
         output = '';
+
     } else if (x === "AC") {
+
         output = String(output).slice(0, -1);
+        
     } else {
+        if (output === "0") {
+            output = "";
+        }
+
         output += x;
     }
 
-    if (output.length > 10 || x.length > 10) {
-        document.querySelector('p').classList.add("result");
-        input.innerHTML = "Max Length";
-
-    } else {
-        input.innerHTML = output;
-        document.querySelector('p').classList.remove("result");
-    }
-
-
-
-
-
-
-
-
-
+    input.innerHTML = output;
 }
+
+
 
 
